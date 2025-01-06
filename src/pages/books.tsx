55163fa64436431484d1aa5c77138b75144f243f
jpmoncao <pedrohjoao44@gmail.com>
2025-01-06 15:05:25 -0300

    feat: campo para pesquisar livro por nome

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate, useSearchParams } from "react-router";
-import { DoorOpenIcon } from "lucide-react";
+import { DoorOpenIcon, Search } from "lucide-react";
 
 import api from "@/services/api";
 import { Button } from "@/components/ui/button";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
 import { Loader } from "@/components/common/loader";
 
 const SkeletonBooks = (props: { width: number }) => (
@@ -27,7 +28,9 @@ export default function Books() {
     const [version, abbrev, chapter] = target?.split('_') ?? [];
 
     const [books, setBooks] = useState([]);
+    const [filteredBooks, setFilteredBooks] = useState([]);
     const [bookAbbrev, setBookAbbrev] = useState(abbrev);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const fetchBooks = async () => {
         const response = await api.get('/books');
@@ -37,7 +40,7 @@ export default function Books() {
 
     const handleSelectedChapter = (index: number) => {
         navigate(`/${version ?? 'nvi'}/${bookAbbrev}/${index}`);
-    }
+    };
 
     useEffect(() => {
         document.title = 'Biblify | Livros';
@@ -45,37 +48,52 @@ export default function Books() {
         (async () => {
             const data = await fetchBooks();
             setBooks(data);
+            setFilteredBooks(data);
         })();
     }, []);
 
+    // Filtra os livros com base no termo de busca
+    useEffect(() => {
+        const filtered = books.filter((book: { name: string }) =>
+            book.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredBooks(filtered);
+    }, [searchTerm, books]);
+
     return (
         <div className="bg-primary-foreground min-h-screen">
-            <header className="bg-primary-foreground py-4 px-4 w-full flex justify-around items-center border-b-[1px] fixed top-0 transition-all duration-200 ease-in h-20">
-                <Link className="group w-4/8" to={`/${version ?? 'nvi'}/${abbrev ?? 'gn'}/${chapter ?? '1'}`}>
-                    <Button className="bg-primary-foreground border border-b-2 border-primary text-primary hover:text-primary-foreground hover:bg-primary"><DoorOpenIcon /> Voltar</Button>
-                </Link>
+            <header className="flex flex-col gap-6 w-full bg-primary-foreground border-b-[1px] p-4 fixed top-0 transition-all duration-200 ease-in h-20">
+                <div className="w-full flex justify-around items-center ">
+                    <Link className="group w-4/8" to={`/${version ?? 'nvi'}/${abbrev ?? 'gn'}/${chapter ?? '1'}`}>
+                        <Button className="bg-primary-foreground border border-b-2 border-primary text-primary hover:text-primary-foreground hover:bg-primary">
+                            <DoorOpenIcon /> Voltar
+                        </Button>
+                    </Link>
 
-                <h1 className="text-primary text-center font-semibold">Livros</h1>
+                    <h1 className="text-primary text-center font-semibold">Livros</h1>
 
-                <Button className="w-4/8 opacity-0"><DoorOpenIcon /> Voltar</Button>
+                    <Button className="w-4/8 opacity-0"><DoorOpenIcon /> Voltar</Button>
+                </div>
             </header>
-            <main className="mt-20 mb-8 w-full max-w-[880px] mx-auto bg-primary-foreground">
-                {books && books.length === 0 && (
+            <main className="mt-24 mb-8 w-full max-w-[880px] mx-auto bg-primary-foreground">
+                <div className="w-full flex items-center justify-center">
+                    <Search className="relative text-secondary-foreground left-8" size={24} />
+                    <Input
+                        placeholder="Buscar livro..."
+                        className="indent-8 -ml-1 w-11/12 my-4 max-w-[400px] bg-secondary text-secondary-foreground"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.currentTarget.value)}
+                    />
+                </div>
+                {filteredBooks && filteredBooks.length === 0 && (
                     <div className="pt-4">
                         <Loader />
-                        <SkeletonBooks width={2} />
-                        <SkeletonBooks width={1} />
-                        <SkeletonBooks width={1} />
-                        <SkeletonBooks width={3} />
-                        <SkeletonBooks width={1} />
-                        <SkeletonBooks width={2} />
-                        <SkeletonBooks width={1} />
                         <SkeletonBooks width={2} />
                         <SkeletonBooks width={1} />
                     </div>
                 )}
                 <Accordion type="single" collapsible className="w-full" onValueChange={(event) => setBookAbbrev(event)}>
-                    {books && books.map((book: { abbrev: { pt: string }, name: string, chapters: number }) => (
+                    {filteredBooks && filteredBooks.map((book: { abbrev: { pt: string }, name: string, chapters: number }) => (
                         <div key={book.abbrev.pt}>
                             <AccordionItem value={book.abbrev.pt}>
                                 <AccordionTrigger className="px-4 py-4 w-full text-left text-lg text-primary">
@@ -97,6 +115,6 @@ export default function Books() {
                     ))}
                 </Accordion>
             </main>
-        </div >
-    )
+        </div>
+    );
 }
