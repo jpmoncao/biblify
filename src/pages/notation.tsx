a277e6a08a20fba0a9dff0050702e8f00a824cdc
jpmoncao <pedrohjoao44@gmail.com>
2025-01-08 11:07:42 -0300

    feat: funções do caderno de anotações

--- src/pages/notation.tsx
+++ src/pages/notation.tsx
@@ -2,14 +2,15 @@ import { Link, useSearchParams } from "react-router"
 import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import Editor from "@/components/note/editor";
 
 export default function Notation() {
     const [searchParams, _] = useSearchParams();
     const data = searchParams.get('date')?.split('-') ?? ['', '', ''];
 
     return (
-        <main className="min-h-screen flex flex-col gap-4 pt-4 max-w-[880px] mx-auto transition-all animate-opacity">
-            <div className="">
+        <main className="min-h-screen flex flex-col gap-4 pt-4 px-4 max-w-[1200px] mx-auto transition-all animate-opacity">
+            <header className="flex flex-col gap-4">
                 <Link to={`/calendar?_target=${(data.join('-'))}`}>
                     <Button
                         className="self-start border border-b-2 border-primary bg-background text-primary hover:text-primary-foreground"
@@ -17,9 +18,10 @@ export default function Notation() {
                         <ArrowLeft /> Voltar
                     </Button>
                 </Link>
-            </div>
-            <h1 className="text-2xl">Devocional do dia {data[2].padStart(2, '0') + '/' + data[1].padStart(2, '0') + '/' + data[0]}</h1>
-            <Separator className="bg-black opacity-50 h-[2px] rounded-md" />
+                <h1 className="text-2xl text-primary">Devocional do dia <strong>{data[2].padStart(2, '0') + '/' + data[1].padStart(2, '0') + '/' + data[0]}</strong></h1>
+                <Separator className="bg-black opacity-50 h-[2px] rounded-md" />
+            </header>
+            <Editor />
         </main>
     )
 }
