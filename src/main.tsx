b87806724a1ded0bbc43ffdd3516f1650b04743e
jpmoncao <pedrohjoao44@gmail.com>
2025-01-14 14:45:34 -0300

    fix: strict mode removido

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
@@ -7,8 +6,8 @@ import { Toaster } from '@/components/ui/toaster'
 import App from '@/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <Toaster />
     <App />
-  </StrictMode>
+  </>
 )
