d6ca54cbe10da95061552915d770bcc853ff3d47
jpmoncao <pedrohjoao44@gmail.com>
2025-01-22 09:38:40 -0300

    feat: rotas autenticadas

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,37 +1,38 @@
-// Importing necessary libraries
 import express from 'express';
 import dotenv from 'dotenv';
-
-// Importing database connection
+import cors from 'cors';
 import connectDB from './database/conn';
-
-// Importing routers
 import userRouter from './routers/user';
 import versesRouter from './routers/verse';
 import notesRouter from './routers/note';
+import checkTokenMiddleware from './middlewares/check-token';
 
-// Load environment variables
 dotenv.config();
-
-// Initialize Express app
 const app = express();
 
-// Middleware for parsing JSON
+// Middleware
 app.use(express.json());
+app.use(cors());
 
-// Apply connection with database
-connectDB();
+// Conexão com o banco de dados
+(async () => {
+    try {
+        await connectDB();
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Database connection failed:', error);
+        process.exit(1);
+    }
+})();
 
-// Routes
-app.use('/api/users', userRouter)
-app.use('/api/verses', versesRouter)
-app.use('/api/notes', notesRouter)
+// Teste de saúde da API
+app.get('/', (req, res) => { res.status(201).json({ message: 'API is running!' }) });
 
-// Route to test if API is up
-app.get('/', (req, res) => {
-    res.send('API is running');
-});
+// Rotas
+app.use('/api/users', userRouter);
+app.use('/api/verses', checkTokenMiddleware, versesRouter);
+app.use('/api/notes', checkTokenMiddleware, notesRouter);
 
-// Start the server
+// Inicialização do Servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
