7736a12367aceae951ad2a87c4e0c7271c03cccf
jpmoncao <pedrohjoao44@gmail.com>
2025-01-22 09:38:33 -0300

    feat: rota de login e criação de conta

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -1,7 +1,11 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import IUser from "../interfaces/user";
 import User from "../models/user";
 
 const saveUser = async (data: IUser): Promise<IUser> => {
+    data.password = await bcrypt.hash(data.password, 8);
+
     const user = new User(data);
     const savedUser: IUser = await user.save();
     return savedUser;
@@ -12,4 +16,22 @@ const listUsers = async (): Promise<IUser[]> => {
     return users;
 }
 
-export { saveUser, listUsers }
+const loginUser = async (email: string, password: string): Promise<string> => {
+    const user = await User.findOne({ email });
+
+    if (!user)
+        throw new Error("User not found");
+
+    if (!(await bcrypt.compare(password, user.password)))
+        throw new Error("Invalid password");
+
+    const token = jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET ?? "",
+        { expiresIn: "60d" }
+    );
+
+    return token;
+}
+
+export { saveUser, listUsers, loginUser }
--- api/src/routers/user.ts
+++ api/src/routers/user.ts
@@ -1,16 +1,16 @@
-import { Router } from "express"
+import { Request, Response, Router } from "express"
 import IUser from "../interfaces/user";
-import { saveUser, listUsers } from "../controllers/user";
+import { saveUser, listUsers, loginUser } from "../controllers/user";
 
 const userRouter = Router();
 
 // CRUD routes
 // Create
-userRouter.post('/', async (req, res) => {
+userRouter.post('/', async (req: Request, res: Response) => {
     try {
-        const { name, email }: IUser = req.body;
+        const { name, email, password }: IUser = req.body;
 
-        const savedUser = await saveUser({ name, email });
+        const savedUser = await saveUser({ name, email, password });
 
         res.status(201).json({ data: savedUser, message: 'User sucessfully created!' });
     } catch (error) {
@@ -18,8 +18,20 @@ userRouter.post('/', async (req, res) => {
     }
 });
 
+userRouter.post('/login', async (req: Request, res: Response) => {
+    try {
+        const { email, password }: IUser = req.body;
+
+        const token = await loginUser(email, password);
+
+        res.status(201).json({ data: { token }, message: 'User sucessfully login!' });
+    } catch (error) {
+        res.status(400).json({ error: (error as Error).name ?? '', message: (error as Error).message });
+    }
+});
+
 // Read
-userRouter.get('/', async (req, res) => {
+userRouter.get('/', async (req: Request, res: Response) => {
     try {
         const users = await listUsers();
 
