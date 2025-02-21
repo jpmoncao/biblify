4dbb926f1a514e08830afd34b4409ea57dc7b16a
jpmoncao <pedrohjoao44@gmail.com>
2025-02-21 10:30:31 -0300

    feat: onClick e className dinâmicos no componente

--- client/src/components/common/back-button.tsx
+++ client/src/components/common/back-button.tsx
@@ -1,17 +1,27 @@
 import { ButtonHTMLAttributes } from "react";
 import { useNavigate, useLocation } from "react-router";
 import { DoorOpenIcon } from "lucide-react";
+import { backNavigate } from "@/utils/navigate";
 import { Button } from '@/components/ui/button';
 
 const BackButton: React.FC<ButtonHTMLAttributes<HTMLButtonElement>> = (props) => {
-    const { children, ...rest } = props;
+    const { children, onClick, className, ...rest } = props;
+
     const navigate = useNavigate();
     const location = useLocation();
 
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        if (onClick) {
+            onClick(event);
+        } else {
+            backNavigate(navigate, location);
+        }
+    };
+
     return (
         <Button
-            className="bg-primary-foreground border border-b-2 border-primary text-primary hover:text-primary-foreground hover:bg-primary"
-            onClick={() => location.key ? navigate(-1) : navigate("/")}
+            className={`${className} bg-primary-foreground border border-b-2 border-primary text-primary hover:text-primary-foreground hover:bg-primary`}
+            onClick={(event) => handleClick(event)}
             {...rest}
         >
             {children ?? (
