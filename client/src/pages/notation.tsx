aac5a33cf2a82308fb8b149a1c6211c16bfc7dfb
jpmoncao <pedrohjoao44@gmail.com>
2025-02-21 22:08:12 -0300

    feat: componentizando a header das telas

--- client/src/pages/notation.tsx
+++ client/src/pages/notation.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link, useSearchParams, useNavigate } from "react-router";
-import { ArrowLeft, Check, Settings } from "lucide-react";
+import { Check, ChevronLeft, Settings } from "lucide-react";
 import { apiAccount } from "@/services/api";
 import { NotationProvider, useNotationContext } from "@/contexts/notation";
 import { useSettingsContext } from "@/contexts/settings";
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import Editor from "@/components/note/editor";
 import { Loader } from "@/components/common/loader";
+import BackButton from "@/components/common/back-button";
 
 function NotationContent() {
     const navigate = useNavigate();
@@ -50,11 +51,9 @@ function NotationContent() {
         <main className="min-h-screen flex flex-col gap-4 pt-4 px-4 max-w-[1200px] mx-auto transition-all animate-opacity">
             <header className="flex flex-col gap-4">
                 <div className="flex justify-between items-center">
-                    <Link to={`/calendar?_target=${data.join('-')}`}>
-                        <Button className="self-start border border-b-2 border-primary bg-background text-primary hover:text-primary-foreground">
-                            <ArrowLeft /><span className="hidden xs:block">Voltar</span>
-                        </Button>
-                    </Link>
+                    <BackButton className="justify-start w-auto pl-1 -ml-2 pr-2">
+                        <ChevronLeft /> <span className="hidden xs:block">Voltar</span>
+                    </BackButton>
                     <div className="flex items-center gap-4 ">
                         <Button className="h-6 bg-transparent border text-primary border-primary opacity-60 hover:bg-primary hover:text-primary-foreground cursor-default">
                             <span className="text-xs flex gap-2 justify-between">
--- client/src/pages/versions.tsx
+++ client/src/pages/versions.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { SaveIcon } from "lucide-react";
 import { apiBible } from "@/services/api";
-import { Button } from "@/components/ui/button";
+import { useSettingsContext } from "@/contexts/settings";
 import { Separator } from "@/components/ui/separator";
 import { Loader } from "@/components/common/loader";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
-import BackButton from "@/components/common/back-button";
-import { useSettingsContext } from "@/contexts/settings";
+import { Header } from "@/components/common/header";
 
 const fetchVersions = async () => {
     const response = await apiBible.get('/versions');
@@ -34,14 +32,10 @@ export default function Versions() {
     const navigate = useNavigate();
 
     const { settings } = useSettingsContext();
-    const { version, book, chapter } = settings().lastBookChapter;
+    const { version } = settings().lastBookChapter;
 
     const [newVersion, setNewVersion] = useState(version);
     const [versions, setVersions] = useState([]);
-
-    function handleSave() {
-        navigate(`/${newVersion}/${book}/${chapter}`);
-    }
 
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: "instant" });
@@ -56,16 +50,10 @@ export default function Versions() {
 
     return (
         <div className="animate-opacity">
-            <header className="bg-background py-4 px-4 w-full flex justify-around items-center border-b-[1px] fixed top-0 transition-all duration-200 ease-in h-20">
-                <BackButton />
-
-                <h1 className="text-primary text-center font-semibold">Versões</h1>
-
-                <Button
-                    className="group w-4/8 hover:bg-primary border border-b-2 border-primary hover:text-primary-foreground text-secondary-foreground bg-secondary"
-                    onClick={handleSave}
-                ><SaveIcon /><span className="hidden xs:block">Salvar</span></Button>
-            </header>
+            <Header 
+                title="Versões"
+                fnBackButton={() => navigate('/' + newVersion)}
+            />
 
             <main className="mt-20 mb-12 w-full max-w-[880px] mx-auto">
                 <RadioGroup onValueChange={(event) => setNewVersion(event)} className="gap-0">
