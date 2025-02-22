e3d60e2f8b47b1e8f707ef69d70e4465e6169d9a
jpmoncao <pedrohjoao44@gmail.com>
2025-02-21 22:06:13 -0300

    feat: alterando tamanho e z-indx

--- client/src/components/common/header.tsx
+++ client/src/components/common/header.tsx
@@ -10,13 +10,13 @@ export function Header(props: IHeaderProps) {
     const TAMANHO_DIV_BACK_BUTTON = 12;
 
     return (
-        <header className="bg-background py-4 px-4 w-full flex items-center border-b-[1px] fixed top-0 transition-all duration-200 ease-in h-20">
+        <header className="bg-background py-4 px-4 w-full flex items-center border-b-[1px] fixed top-0 transition-all duration-200 ease-in h-20 z-50">
             <div className={`ml-auto flex-none w-${TAMANHO_DIV_BACK_BUTTON}`}>
                 {!props.hiddenBackButton && <BackButton onClick={props.fnBackButton} />}
             </div>
 
             {/* pr-12 é o tamanho da div do BackButton (w-12) */}
-            <div className={`flex-grow text-center pr-${TAMANHO_DIV_BACK_BUTTON} max-w-[800px] mr-auto`}>
+            <div className={`flex-grow text-center pr-${TAMANHO_DIV_BACK_BUTTON} max-w-[840px] mr-auto`}>
                 <h1 className="text-primary font-semibold">{props.title}</h1>
             </div>
         </header>
