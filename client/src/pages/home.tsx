cb9d1e4de5d131ec6d00ce73f41194121621cb25
jpmoncao <pedrohjoao44@gmail.com>
2025-02-24 17:24:30 -0300

    feat: alterou a home

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
-import { ArrowRight, BookMarked, FolderGit, Notebook, Settings, CircleUserRound } from "lucide-react";
+import { ArrowRight, FolderGit } from "lucide-react";
 import { getVerseOfDay } from "@/utils/verse-of-day";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
-import useSettings from "@/hooks/use-settings";
+import { useSettingsContext } from "@/contexts/settings";
 
 interface Verse {
     abbrev: string;
@@ -16,7 +16,7 @@ interface Verse {
 }
 
 export default function Home() {
-    const { settings } = useSettings();
+    const { settings } = useSettingsContext();
     const [verseOfDay, setVerseOfDay] = useState<Verse | null>(null);
 
     useEffect(() => {
@@ -29,7 +29,7 @@ export default function Home() {
     }, []);
 
     return (
-        <div className="p-4 pt-6 w-full max-w-[880px] mx-auto flex flex-col gap-4 animate-opacity">
+        <div className="p-4 pt-6 w-full max-w-[880px] mx-auto flex flex-col gap-4 animate-opacity min-h-[92vh]">
             <div className="flex items-center justify-between gap-4">
                 <h1 className="text-2xl text-foreground font-semibold">
                     Bem-vindo ao <span className="text-3xl text-foreground font-bold">Biblify</span>
@@ -40,41 +40,12 @@ export default function Home() {
                             <Button variant={'secondary'} className="aspect-square px-3 py-1 border border-primary hover:bg-primary hover:text-primary-foreground border-b-2 flex items-center">Fazer Login</Button>
                         </Link>
                     }
-                    <Link to={`/settings`} className="group inline-block justify-self-end">
-                        <Button className="aspect-square p-1 bg-primary-foreground border border-b-2 border-primary text-foreground group-hover:text-primary group-hover:bg-primary flex items-center">
-                            <Settings className="group-hover:text-primary-foreground" /></Button>
-                    </Link>
                 </div>
             </div>
             <p className="text-foreground max-md:max-w-[500px]">
                 Aqui você tem acesso ao livro mais impactante e poderoso de todos os tempos:{" "}
                 <strong>a Bíblia</strong>.
             </p>
-
-            <Separator className="my-2" />
-
-            <div className="flex justify-between items-center">
-                <Link to={`/${settings().lastBookChapter.version}/${settings().lastBookChapter.book}/${settings().lastBookChapter.chapter}`} className="group inline-block h-24 w-24">
-                    <Button className="p-0 bg-primary-foreground border border-b-2 border-primary text-foreground group-hover:text-primary-foreground foreground group-hover:bg-primary flex flex-col h-full w-full [&_svg]:size-14">
-                        <BookMarked className="group-hover:text-primary-foreground items-center justify-center" />
-                        <span className="group-hover:text-primary-foreground">Ler a Bíblia</span>
-                    </Button>
-                </Link>
-
-                <Link to="/note" className="group inline-block h-24 w-24">
-                    <Button className="p-0 bg-primary-foreground border border-b-2 border-primary text-foreground group-hover:text-primary-foreground foreground group-hover:bg-primary flex flex-col h-full w-full [&_svg]:size-14">
-                        <Notebook className="group-hover:text-primary-foreground items-center justify-center" />
-                        <span className="group-hover:text-primary-foreground">Anotações</span>
-                    </Button>
-                </Link>
-
-                <Link to={settings().token ? '/profile' : '/login'} className="group inline-block h-24 w-24">
-                    <Button className="p-0 bg-primary-foreground border border-b-2 border-primary text-foreground group-hover:text-primary-foreground foreground group-hover:bg-primary flex flex-col h-full w-full [&_svg]:size-14">
-                        <CircleUserRound className="group-hover:text-primary-foreground items-center justify-center" />
-                        <span className="group-hover:text-primary-foreground">Meu perfil</span>
-                    </Button>
-                </Link>
-            </div>
 
             <Separator className="my-2" />
 
@@ -112,10 +83,10 @@ export default function Home() {
 
             <div className="border-l-4 py-2 pl-4 border-primary bg-primary-foreground rounded-md shadow-sm">
                 <h2 className="text-lg text-foreground font-semibold mb-2">Mensagem do desenvolvedor:</h2>
-                <p className="text-md text-foreground ">
+                <p className="text-sm text-foreground ">
                     Que esta ferramenta possa ser benção na sua vida! Obrigado por acessá-la...
                 </p>
-                <p className="text-md text-foreground ">
+                <p className="text-sm text-foreground ">
                     Fique à vontade para dar uma estrela no github ou me ajudar com sugestões/melhorias para o projeto.
                 </p>
                 <a rel="noopener" href="https://github.com/jpmoncao/biblify" className="group inline-block mt-4" target="_blank">
@@ -128,7 +99,7 @@ export default function Home() {
 
             <Separator className="mt-2" />
 
-            <footer className="mt-auto text-xs text-foreground text-zinc-text-foreground 400 text-center font-Inter">
+            <footer className="mt-auto text-xs text-foreground/30 text-center font-Inter">
                 Desenvolvido por João Pedro Monção - 2025
             </footer>
         </div >
