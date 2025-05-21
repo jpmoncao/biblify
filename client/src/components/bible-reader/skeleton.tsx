de2f83ac6ecb709288772e741c8f573b0aac318e
jpmoncao <pedrohjoao44@gmail.com>
2025-05-21 20:29:27 -0300

    fix: alinhando da forma correta o skeleton

--- client/src/components/bible-reader/skeleton.tsx
+++ client/src/components/bible-reader/skeleton.tsx
@@ -2,21 +2,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 const SkeletonVerses = () => (
     <div className="flex flex-col align-center gap-2">
-        <Skeleton className="h-4 w-12-12" />
-        <Skeleton className="h-4 w-10/12" />
-        <Skeleton className="h-4 w-11/12" />
-        <Skeleton className="h-4 w-9/12" />
-        <Skeleton className="h-4 w-9/12" />
-        <Skeleton className="h-4 w-11/12" />
-        <Skeleton className="h-4 w-12/12" />
-        <Skeleton className="h-4 w-12/12 mb-2" />
+        <Skeleton className="h-[1em] w-12-12" />
+        <Skeleton className="h-[1em] w-10/12" />
+        <Skeleton className="h-[1em] w-11/12" />
+        <Skeleton className="h-[1em] w-9/12" />
+        <Skeleton className="h-[1em] w-9/12" />
+        <Skeleton className="h-[1em] w-11/12" />
+        <Skeleton className="h-[1em] w-12/12" />
+        <Skeleton className="h-[1em] w-12/12 mb-2" />
     </div>
 );
 
 export default function BibleReaderSkeleton() {
     return (
         <>
-            <main className="bg-background mt-4 mb-12 px-4 w-full max-w-[880px] mx-auto">
+            <main className="bg-background mt-5 mb-12 px-4 w-full max-w-[880px] mx-auto">
                 <SkeletonVerses />
                 <SkeletonVerses />
                 <SkeletonVerses />
