8da7d935139dfdae29de92e7a34da176a5f385a7
jpmoncao <pedrohjoao44@gmail.com>
2025-05-21 13:52:17 -0300

    feat: removendo os providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,11 @@
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import App from '@/App.tsx';
-import { SettingsProvider } from '@/contexts/settings';
 import { Toaster } from '@/components/ui/toaster';
 
 createRoot(document.getElementById('root')!).render(
   <>
-    <SettingsProvider>
-      <Toaster />
-      <App />
-    </SettingsProvider>
+    <Toaster />
+    <App />
   </>
 )
